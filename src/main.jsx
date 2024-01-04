5a08fa9d34e4e49e45a7eebb9d55a661794b7681
DAMOCLOIDS <khoa27012000@gmail.com>
2024-01-04 21:31:05 +0700

    style: add theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './pages/Home/Home'
+import { experimental_extendTheme as extendTheme } from '@mui/material'
+import { Experimental_CssVarsProvider as MuiProvider } from '@mui/material'
+
+const theme = extendTheme({
+  colorSchemes: {
+    light: {},
+    dark: {}
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <MuiProvider theme={theme}>
+      <Home />
+    </MuiProvider>
   </React.StrictMode>
 )
