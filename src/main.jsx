3518c8d0e88431828a3730130b8429dd16658ddd
DAMOCLOIDS <khoa27012000@gmail.com>
2024-01-04 21:45:07 +0700

    chore: add theme & router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './pages/Home/Home'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { experimental_extendTheme as extendTheme } from '@mui/material'
+import { Experimental_CssVarsProvider as MuiProvider } from '@mui/material'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  }
+])
+
+const theme = extendTheme({
+  nhkhoa: {},
+  colorSchemes: {
+    light: {
+      palette: {}
+    },
+    dark: {
+      palette: {}
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <MuiProvider theme={theme} />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
