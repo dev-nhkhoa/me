2cf30abecbd6b366edcb856ade187ec4e19fc1ca
dev-nhkhoa <work.nhkhoa@gmail.com>
2024-06-09 13:01:30 -0700

    chore: Update npm dependencies and add @vercel/analytics package

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import { RouterProvider } from 'react-router-dom'
 
 import { Experimental_CssVarsProvider as MuiProvider } from '@mui/material'
 
+import { Analytics } from '@vercel/analytics/react'
+
 import './App.css'
 import { theme } from './theme'
 import { router } from './Router'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <MuiProvider theme={theme}>
+    <Analytics />
     <RouterProvider router={router} />
   </MuiProvider>
 )
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,15 @@
+import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 
+import LogoImage from '~/assets/images/nhkhoa-logo-nobg.png'
+
 function Logo() {
-  return <Typography sx={{ textDecoration: 'underline', color: 'primary.text.primary', cursor: 'pointer' }}>Nhkhoa</Typography>
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <img src={LogoImage} style={{ width: '32px', height: '32px' }} />
+      <Typography sx={{ textDecoration: 'underline', color: 'primary.text.primary', cursor: 'pointer' }}>Nhkhoa</Typography>
+    </Box>
+  )
 }
 
 export default Logo
